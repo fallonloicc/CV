bb0d1eb904dc4a69a869641a3d6362f648cd0e36
Loïc Fallon <thebose@live.fr>
2019-03-20 11:11:02 +0100

    accès bdd

--- PPE2/SiteVitrine/params/db.php
+++ PPE2/SiteVitrine/params/db.php
@@ -3,8 +3,8 @@
 	/* DEBUT D'INITIALISATION DE LA CONNEXION DB */
 	try
 	{
-		//$bdd = new PDO('mysql:host=localhost;dbname=efficom_loic', "loicfallon", "efficom");
-		$bdd = new PDO('mysql:host=localhost;dbname=loicgregloick', "root", "");
+		$bdd = new PDO('mysql:host=localhost;dbname=efficom_loic', "loicfallon", "efficom");
+		//$bdd = new PDO('mysql:host=localhost;dbname=loicgregloick', "root", "");
 		$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 	}
 	catch(PDOException $e)
