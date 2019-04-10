d67c96e681929c0f6a0f5f997c78e6b5ef591e40
Loïc Fallon <thebose@live.fr>
2019-04-10 18:35:14 +0200

    Modification lien base de donnée

--- PPE2/API/db.php
+++ PPE2/API/db.php
@@ -4,8 +4,8 @@ function getPdo()
 {
     try
     {
-        //$bdd = new PDO('mysql:host=localhost;dbname=efficom_loic', "loicfallon", "efficom");
-        $bdd = new PDO('mysql:host=localhost;dbname=loicgregloick', "root", "");
+        $bdd = new PDO('mysql:host=localhost;dbname=efficom_loic', "loicfallon", "efficom");
+        //$bdd = new PDO('mysql:host=localhost;dbname=loicgregloick', "root", "");
         $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         return $bdd;
     }
