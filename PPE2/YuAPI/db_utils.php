b945a8bc8243bba052690a20977aad13ba655f02
Loïc Fallon <thebose@live.fr>
2019-05-08 17:36:19 +0200

    no message

--- PPE2/YuAPI/db_utils.php
+++ PPE2/YuAPI/db_utils.php
@@ -9,17 +9,13 @@
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // function getDuelByCode($code)
-    // {
-    //     $req = <<<
-    //     SELECT * FROM duel
-    //     WHERE player1 = :code
-    //     OR player2 = :code
-    // EOD;
-    //
-    //     $statement = getPdo()->prepare($req);
-    //     $statement->execute(array(":code" => $code));
-    //     return $statement->fetchAll(PDO::FETCH_ASSOC);
-    // }
+    function getDuelByCode($code)
+    {
+         $req = 'SELECT * FROM duel WHERE player1 ==:code OR player2 ==:code
+
+         $statement = getPdo()->prepare($req);
+         $statement->execute(array(":code" => $code));
+         return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 ?>
