9bc7ddcf8751ba0cffbe4761fe7d32019cd3fa1f
Loïc Fallon <thebose@live.fr>
2019-05-08 17:46:00 +0200

    no message

--- PPE2/YuAPI/duelById.php
+++ PPE2/YuAPI/duelById.php
@@ -3,7 +3,7 @@
     
     function getDuelByCode($code)
     {
-        $req = 'SELECT * FROM duel WHERE player1 =="'.$code.'" OR player2 =="'.$code.'"';
+        $req = 'SELECT * FROM duel WHERE player1 ="'.$code.'" OR player2 ="'.$code.'"';
 
          $statement = getPdo()->prepare($req);
          $statement->execute();
