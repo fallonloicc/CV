3471bfb674c44f60960dacb969e28b36155f86a9
Loïc Fallon <thebose@live.fr>
2019-05-08 03:55:01 +0200

    .

--- PPE2/YuAPI/db.php
+++ PPE2/YuAPI/db.php
@@ -2,11 +2,9 @@
 
     function getPdo()
     {
-        echo "1";
         $dsn = "mysql:host=localhost;dbname=efficom_yugioh";
-        echo "2";
         $dbh = new PDO($dsn, "loicfallon", "efficom", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-        echo "3"
         return $dbh;
+        
         echo "c'est branché !";
     }
