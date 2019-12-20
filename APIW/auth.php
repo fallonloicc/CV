3c91b6b7f4a0bff305ddc76219d968af385c3332
fallonloic <fallonloic@hotmail.fr>
2019-12-20 10:44:10 +0100

    Update

--- APIW/auth.php
+++ APIW/auth.php
@@ -1,9 +1,9 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: access");
-    header("Access-Control-Allow-Methods: GET");
-    header("Access-Control-Allow-Credentials: true");
-    header('Content-Type: application/json');
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     include_once 'dbclass.php';
     include_once 'user.php';
@@ -15,7 +15,6 @@
     $user = new User($db);
 
     $data = json_decode(file_get_contents("php://input"));
-    echo $data ->email;
     // query to read single record
     $query = "SELECT * FROM user WHERE email = :email";
     
